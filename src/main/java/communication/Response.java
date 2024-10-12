3a3363c2b493bc53031ef0c5938254c3c401cda6
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-10-12 15:14:54 +0200

    implemented a communication protocol

--- src/main/java/communication/Response.java
+++ src/main/java/communication/Response.java
@@ -1,5 +1,30 @@
 package communication;
 
+import kong.unirest.json.JSONObject;
+
 public class Response {
     
+    public static String register(String status, String message) {
+        JSONObject data = new JSONObject();
+        JSONObject response = new JSONObject();
+
+        data.put("message", message);
+
+        response.put("status", status);
+        response.put("data", data);
+
+        return response.toString();
+    }
+
+    public static String login(String status, String message) {
+        JSONObject data = new JSONObject();
+        JSONObject response = new JSONObject();
+
+        data.put("message", message);
+
+        response.put("status", status);
+        response.put("data", data);
+
+        return response.toString();
+    }
 }
