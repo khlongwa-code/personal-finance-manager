29f76fba26dde59b8ba055bedc07950fbe625e39
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-10-12 15:11:43 +0200

    implemented a user input class

--- src/main/java/client/UserInputs.java
+++ src/main/java/client/UserInputs.java
@@ -1,5 +1,94 @@
 package client;
 
+import communication.Request;
+
+import java.util.Scanner;
+
 public class UserInputs {
-    
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private final UserInputsValidator validator;
+    private Request request;
+    private Scanner scanner;
+
+    public UserInputs() {
+        validator = new UserInputsValidator();
+        request = new Request();
+        scanner = new Scanner(System.in);
+    }
+
+    public String registrationInfo() {
+        String firstName = getUserFirstName();
+        String lastName = getUserLastName();
+        String email = getUserEmail();
+
+        return request.register(firstName, lastName, email);
+    }
+
+    public String loginInfo() {
+        String email = getUserEmail();
+        String password = getUserPassword();
+
+        return request.login(email, "null");
+    }
+
+    public String userCommands() {
+        return "";
+    }
+
+    public String getUserFirstName() {
+
+        while (true) {
+            System.out.println("Enter your first name: ");
+            this.firstName = scanner.nextLine();
+            if (validator.isValidName(firstName)) {
+                return firstName;
+            } else {
+                System.out.println("Invalid first name. Please enter a valid name");
+            }
+        }
+    }
+
+    public String getUserLastName() {
+
+        while (true) {
+            System.out.println("Enter your last name: ");
+            this.lastName = scanner.nextLine();
+            if (validator.isValidName(lastName)) {
+                return lastName;
+            } else {
+                System.out.println("Invalid last name. Please enter a valid  last name");
+            }
+        }
+    }
+
+    public String getUserEmail() {
+
+        while (true) {
+            System.out.println("Enter your email: ");
+            this.email = scanner.nextLine();
+
+            if (validator.isValidEmail(email)) {
+                return email;
+            } else {
+                System.out.println("Invalid email. Please enter a valid email");
+            }
+        }
+    }
+
+    public String getUserPassword() {
+        return null;
+    }
+
+    public String handleUserInputs(String userInput) {
+        if (userInput.equals("register")) {
+            return registrationInfo();
+        } else if (userInput.equals("login")) {
+            return loginInfo();
+        } else {
+            return userCommands();
+        }
+    }
 }
