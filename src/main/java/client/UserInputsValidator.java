f7863742c3d98d82b8d41f5a311d688f1a176a9b
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-10-12 15:13:04 +0200

    implemedted a user input validator class

--- src/main/java/client/UserInputsValidator.java
+++ src/main/java/client/UserInputsValidator.java
@@ -1,5 +1,27 @@
 package client;
 
 public class UserInputsValidator {
-    
+    /**
+     * Validates if the name contains only alphabets.
+     *
+     * @param name The name to validate
+     * @return true if the name is all alphabets, otherwise false
+     */
+    public boolean isValidName(String name) {
+        return name.matches("^[a-zA-Z]+$");
+    }
+
+    /**
+     * Validates if the email is in the correct format.
+     *
+     * @param email The email to validate
+     * @return true if the email is in the correct format, otherwise false
+     */
+    public boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        return email.matches(emailRegex);
+    }
 }
